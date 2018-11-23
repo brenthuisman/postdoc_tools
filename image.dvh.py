8cfe862f672934dc1f8603dc55df4de64fcf8b70
Brent Huisman <mail@brenthuisman.net>
2018-11-23 17:04:29 +0100

    gamma tuning/update. npgamma still is fastest

--- image.dvh.py
+++ image.dvh.py
@@ -5,22 +5,22 @@ from os import path
 parser = argparse.ArgumentParser(description='supply an image and a mask or percentage for isodose contour in which to compute the DVH.')
 parser.add_argument('inputimage')
 parser.add_argument('--maskimage',default=None)
-parser.add_argument('--isodoseregion',default=None,type=float)
+parser.add_argument('--maskregion',default=None,type=float)
 opt = parser.parse_args()
 
 im = image.image(path.abspath(opt.inputimage))
 maskim = None
 
-if opt.isodoseregion == None and path.isfile(path.abspath(opt.maskimage)):
+if opt.maskregion == None and path.isfile(path.abspath(opt.maskimage)):
     print('Using',opt.maskimage,'as region for DVH analysis.')
     maskim = image.image(path.abspath(opt.maskimage))
-elif opt.isodoseregion != None:
-    assert 0 < opt.isodoseregion < 100
-    print('Using isodose contour at',opt.isodoseregion,'percent of maximum dose as region for DVH analysis.')
+elif opt.maskregion != None:
+    assert 0 < opt.maskregion < 100
+    print('Using isodose contour at',opt.maskregion,'percent of maximum dose as region for DVH analysis.')
     maskim = im.copy()
-    maskim.tomask_atthreshold((opt.isodoseregion/100.)*maskim.max())
+    maskim.tomask_atthreshold((opt.maskregion/100.)*maskim.max())
 else:
-    print('No mask or isodoseregion specified; using whole volume for DVH analysis.')
+    print('No mask or maskregion specified; using whole volume for DVH analysis.')
 
 if maskim != None:
     im.applymask(maskim)
